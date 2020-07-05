c42cf7f97ca8dcf09ad28ef3b41541e5c234cc26
Dan Draper <dan@coderdan.co>
2020-07-05 22:12:47 +1000

    Added TODO config for ExTodo

---
+++ .todo.exs
@@ -0,0 +1,5 @@
+%ExTodo.Config{
+  error_codetags: ["FIXME", "BUG"],
+  skip_patterns: [~r/\.git/, ~r/_build/, ~r/deps/, ~r/cover/, ~r/doc/, ~r/\.todo\.exs/, ~r/TODO/, ~r/.swp$/],
+  supported_codetags: ["NOTE", "TODO", "FIXME", "HACK", "BUG"]
+}
